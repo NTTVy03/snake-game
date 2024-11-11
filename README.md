c40e165fbec105fa74089da293e55215f9260281
NTTVy03 <trucvy05062003@gmail.com>
2024-11-11 14:31:32 +0700

    use rust compiled pkg in js

---
+++ README.md
@@ -0,0 +1,22 @@
+## Init
+* rustc: 1.82.0 (stable)
+* wasm-pack: v0.13.1
+
+## How to run 
+
+```
+// You are currently at: snake_game/
+
+// Compile the Rust library
+wasm-pack build --target web
+
+// Move to the UI:
+cd www
+
+// Update node modules:
+npm install
+
+// Run website
+npm run dev
+```
+
--- www/index.js
+++ www/index.js
@@ -1,33 +1,8 @@
-async function init() {
-    // init memory in js to use in webassembly
-    const memory = new WebAssembly.Memory({initial: 1});
+import init from "snake_game";
 
-    // import some JS code that we want to use in WebAssembly code
-    const importObject = {
-        js: {
-            mem: memory
-        },
-        console: {
-            log: () => {
-                console.log("Just logging something!");
-            },
-            error: () => {
-                console.log("I am an error");
-            }
-        }
-    }
-
-    // fetch binary code of `.wasm` file
-    const response = await fetch("sum.wasm");
-    const buffer = await response.arrayBuffer();
-
-    const wasm = await WebAssembly.instantiate(buffer, importObject);
-    
-    // note that, the .wasm has changed the memory 
-    
-    const uint8Array = new Uint8Array(memory.buffer, 0, 2); 
-    const hiText = new TextDecoder().decode(uint8Array);
-    console.log(hiText);
-}
-
-init();
+// because the println!() in Rust does not print anything to the console
+// we can not see the greeting message
+// however, this code still works
+init().then( wasm => {
+    wasm.greet("Vy");
+})
