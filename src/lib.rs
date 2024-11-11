f588b76d3db406e4b10f2e7587d23eefe35c9606
NTTVy03 <trucvy05062003@gmail.com>
2024-11-11 15:42:27 +0700

    add wee_alloc

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
 use wasm_bindgen::prelude::*;
+use wee_alloc::WeeAlloc;
+
+// we_alloc makes the wasm file smaller
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
