7c94881fa167786ffb6ee4a494200ddd5476bbb8
NTTVy03 <trucvy05062003@gmail.com>
2024-11-11 13:34:38 +0700

    use webassambly memory in js

--- www/index.js
+++ www/index.js
@@ -17,10 +17,17 @@ async function init() {
 
     const wasm = await WebAssembly.instantiate(buffer, importObject);
 
+    // get functions from webassembly and use it in js
     const sumFunction = wasm.instance.exports.sum;
     const result = sumFunction(5,62);
-
     console.log(result);
+    
+    // get the memory $mem in wasm and use it in js
+    const wasmMemory = wasm.instance.exports.mem;
+    // convert 2 bytes, from byte 0 of wasmMemory into an array
+    const uint8Array = new Uint8Array(wasmMemory.buffer, 0, 2); 
+    const hiText = new TextDecoder().decode(uint8Array);
+    console.log(hiText);
 }
 
 init();
