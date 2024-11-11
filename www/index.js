6cab3cb7c0b8c6d95636cd25a5b00315a4e20e5e
NTTVy03 <trucvy05062003@gmail.com>
2024-11-11 11:56:45 +0700

    use js in webassembly

--- www/index.js
+++ www/index.js
@@ -1,10 +1,22 @@
 async function init() {
+    // import some JS code that we want to use in WebAssembly code
+    const importObject = {
+        console: {
+            log: () => {
+                console.log("Just logging something!");
+            },
+            error: () => {
+                console.log("I am an error");
+            }
+        }
+    }
+
     // fetch binary code of `.wasm` file
     const response = await fetch("sum.wasm");
     const buffer = await response.arrayBuffer();
 
-    const wasm = await WebAssembly.instantiate(buffer);
-    
+    const wasm = await WebAssembly.instantiate(buffer, importObject);
+
     const sumFunction = wasm.instance.exports.sum;
     const result = sumFunction(5,62);
 
