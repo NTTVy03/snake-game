a4c9f221dafd98f5b2c94497e0332f05a89703c1
NTTVy03 <trucvy05062003@gmail.com>
2024-11-11 11:41:25 +0700

    remove index.html from public/

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,10 +1,20 @@
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
+// copy index.js file to the public/index.js
+// and run the server in development mode
 module.exports = {
     entry: "./index.js",
     output: {
         path: path.resolve(__dirname, "public"),
         filename: "index.js"
     },
-    mode: "development"
+    mode: "development",
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: "./index.html", to: "./" }
+            ]
+        })
+    ]
 }
